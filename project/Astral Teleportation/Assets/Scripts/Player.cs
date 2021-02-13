cd1301f2c7feca406aabf913d31f4ef3bdac01d2
Devon Wilkerson <wilkersonrdevon@gmail.com>
2021-02-13 14:11:19 -0500

    Adds testing level and player movement

---
+++ project/Astral Teleportation/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if(Input.GetKey("a"))
+        {
+            pos.x += -(speed) * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
