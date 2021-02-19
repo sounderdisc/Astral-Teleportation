48a49b8786562da12e17bc75b2d6c516aab9a4b7
sounderdisc <mattbhubbs@knights.ucf.edu>
2021-02-19 17:50:52 -0500

    added play mode tests and an outline for testing PlayerControler

---
+++ project/Astral Teleportation/Assets/Tests/PlayMode/PlayerControlerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerControlerTests
+    {
+        // Assert class api page: https://docs.unity3d.com/ScriptReference/Assertions.Assert.html
+        // "yield return null;" will wait for one frame. "yield return new WaitForSeconds(1);" will wait for 1 second 
+        [UnityTest]
+        public IEnumerator MoveRight()
+        {
+            var gameObject = new GameObject();
+            var player = gameObject.AddComponent<PlayerControler>();
+            yield return new WaitForSeconds(1);
+        }
+
+        [UnityTest]
+        public IEnumerator MoveLeft()
+        {
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator Jump()
+        {
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator stopMovement()
+        {
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator freezePlayerActions()
+        {
+            yield return null;
+        }
+
+    }
+}
