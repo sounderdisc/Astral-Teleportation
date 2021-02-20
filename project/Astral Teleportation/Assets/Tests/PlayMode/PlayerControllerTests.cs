d48a281bf4b5f4361dd115c1d4da6f14facd50c1
Mayank-Patel1 <71153207+Mayank-Patel1@users.noreply.github.com>
2021-02-20 12:16:18 -0500

    Added unit test for movement

--- project/Astral Teleportation/Assets/Tests/PlayMode/PlayerControllerTests.cs
+++ project/Astral Teleportation/Assets/Tests/PlayMode/PlayerControllerTests.cs
@@ -13,13 +13,20 @@ namespace Tests
         {
             GameObject placeholder = new GameObject("placeholderGameObjectt");
             Rigidbody2D rb2d = placeholder.AddComponent<Rigidbody2D>();
+            yield return null;
             rb2d.velocity = new Vector2(0, 0);
             PlayerController player = placeholder.AddComponent<PlayerController>();
-            
+            yield return null;
 
             var startingPos = player.transform.position.x;
-            player.moveRight();
-            yield return new WaitForSeconds(1);
+
+            float timePassed = 0f;
+            while( timePassed < 1){
+                player.moveRight();
+                timePassed += Time.deltaTime;
+                yield return null;
+            }
+            yield return null;
             var endingPos = player.transform.position.x;
             Assert.IsTrue(endingPos > startingPos);
 
