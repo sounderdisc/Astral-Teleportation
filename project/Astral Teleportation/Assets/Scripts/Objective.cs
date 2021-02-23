3cc4624f08d0b91ee005beff378d60c6e08610ef
sounderdisc <mattbhubbs@knights.ucf.edu>
2021-02-23 16:05:53 -0500

    Red and Blue Objectives use the same script
    I left the two old scripts ObjectiveRed.cs and ObjectiveBlue.cs in the scripts folder because I don't want to delete it just yet. Code duplication is bad, and things with the same behavior ought to be different instances of the same class.

---
+++ project/Astral Teleportation/Assets/Scripts/Objective.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Objective : MonoBehaviour
+{
+    public PlayerController player;
+    public Objective otherObjective;
+    public bool isRed;
+    public bool isTaken = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.isDead)
+        {
+            Destroy(gameObject);
+            isTaken = false;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isTaken && ((isRed && collision.gameObject.tag == "RedPlayer") || (!isRed && collision.gameObject.tag == "BluePlayer")))
+        {
+            isTaken = true;
+            gameObject.transform.parent = player.gameObject.transform;
+            Debug.Log(isRed + "got their Objective");
+
+            if (otherObjective.isTaken)
+            {
+                Debug.Log("Both objectives taken, loading next level");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            } 
+            else
+            {
+                Debug.Log("one objective taken. will not load level until both are taken");
+            }
+        }
+    }
+}
