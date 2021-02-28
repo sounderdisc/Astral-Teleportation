fee0fa582fb72699176f8f3b689b89a26e730aaf
Jakob Germann <jakobgermann@comcast.net>
2021-02-28 18:17:53 -0500

    Timer Functionality
    Added a timer that counts up to indicate how long the player is taking on the current level.

---
+++ project/Astral Teleportation/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{ 
+
+    float currentTime;
+    
+
+    [SerializeField] Text countDownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize the timer to the initial time
+        currentTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Decrease current time by 1 in real time, not by fps
+        currentTime += 1 * Time.deltaTime;
+
+        countDownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+
+}
