69b4103e84afb3d3b70d0c0887c3bf7b9f6d14d4
Jakob Germann <jakobgermann@comcast.net>
2021-03-04 22:00:40 -0500

    Button Sounds
    Added a sound for all main menu and level select menu button clicks.  A time delay of 1 second has been placed for each.  This will be temporary until a time delay exactly the length of the sound can be used.

--- project/Astral Teleportation/Assets/Scripts/PlayButton.cs
+++ project/Astral Teleportation/Assets/Scripts/PlayButton.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PlayButton : MonoBehaviour
 {
+
+
+    bool loadingInitiated = false;
+    
+
     // I am expanding this class to also handle the buttons in level select. PlayButtonPressed() may be changed in the future once save data is implemented
     public void PlayButtonPressed()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //If statement is to ensure DelayedLoad() runs only once
+        if (!loadingInitiated)
+        {
+
+            StartCoroutine(DelayedLoad());
+            loadingInitiated = true;
+
+        }
+
+        
     }
 
     public void playLevelFromLevelSelectScreen()
     {
         SceneManager.LoadScene(int.Parse(gameObject.tag));
+    }
+
+
+    IEnumerator DelayedLoad()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
