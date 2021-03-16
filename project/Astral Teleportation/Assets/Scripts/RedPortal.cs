3a03a30bf2ab3ee769d9252e21fc7c8759f53d2b
Mayank-Patel1 <Mayank-Patel1@users.noreply.github.com>
2021-03-15 22:21:33 -0400

    prefab fixes

---
+++ project/Astral Teleportation/Assets/Scripts/RedPortal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public float fireRate = 0f;
+    public LayerMask notToHit;
+
+    float timeToFire = 0f;
+    Transform firepoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firepoint = transform.FindChild("FirePoint");
+        if(firepoint == null){
+            Debug.LogError (" no firepoint found!!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetButtonDown ("Fire1")){
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        Debug.Log("shooting on left click");
+    }
+}
+
