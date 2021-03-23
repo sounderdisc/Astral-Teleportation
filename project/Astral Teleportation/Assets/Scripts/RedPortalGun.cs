e0ffc673acbdcb6cead27c30729e52f0969dfdad
sounderdisc <mattbhubbs@knights.ucf.edu>
2021-03-23 05:18:25 -0400

    Preliminary raytracing added

--- project/Astral Teleportation/Assets/Scripts/RedPortalGun.cs
+++ project/Astral Teleportation/Assets/Scripts/RedPortalGun.cs
@@ -12,6 +12,7 @@ public class RedPortalGun : MonoBehaviour
     public GameObject currentRedPortal;
     public GameObject currentBluePortal;
     public AudioSource portalShootSound;
+    public bool debugPortalMode;
 
     float timeToFire = 0f;
     Transform firepoint;
@@ -25,6 +26,8 @@ public class RedPortalGun : MonoBehaviour
         }
         currentRedPortal = GameObject.Find("RedPortal");
         currentBluePortal = GameObject.Find("BluePortal");
+        // uncomment next line to revert to old fire mode
+        debugPortalMode = true;
     }
 
     // Update is called once per frame
@@ -60,10 +63,72 @@ public class RedPortalGun : MonoBehaviour
     }
 
     void ShootRed() {
-        // Debug.Log("shooting on left click");
-        // we can change this functionality later, but for now, one step at a time
-        SpawnRedAtCursorLocation();
-        portalShootSound.Play();
+        // having trouble with portals shooting while paused. here's a temporty fix
+        if (!PauseMenu.isPaused)
+        {
+            // leaving the old fire mode in for debug purposes, and in case we want to go back and forth
+            if (debugPortalMode)
+            {
+                SpawnRedAtCursorLocation();
+                portalShootSound.Play();
+            }
+            else
+            {
+                Debug.Log("New Fire Mode! Red");
+                SpawnRedUsingRaycast();
+            }
+        }
+    }
+
+    void SpawnRedUsingRaycast()
+    {
+        // find fire direction for the shot
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (Vector2)((worldMousePos - firepoint.position));
+        direction.Normalize();
+        // do raycast, get location of hit
+        RaycastHit2D hit = Physics2D.Raycast(firepoint.position, direction);
+        if (hit.collider == null)
+        {
+            return;
+        }
+        Debug.Log("red raycast hit: " + hit.collider.name);
+        Vector3 spawnLocation = hit.collider.gameObject.transform.position;
+        spawnLocation.z = 662.0625f;
+        GameObject newPortal = Instantiate(redPortalPrefab, spawnLocation, Quaternion.identity);
+        
+        // so first, gonna copy/paste the reference fixing. i must make a "newPortal" before this section
+        // four possible states: no current red and no current blue portal, yes red but no blue, no red but yes blue, and both colors already exist
+        if (currentRedPortal == null && currentBluePortal == null)
+        {
+            currentRedPortal = newPortal;
+        }
+        else if (currentRedPortal != null && currentBluePortal == null)
+        {
+            Destroy(currentRedPortal);
+            currentRedPortal = newPortal;
+        }
+        else if (currentRedPortal == null && currentBluePortal != null)
+        {
+            PortalCollision newRedPortalScript = newPortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+            PortalCollision bluePortalScript = currentBluePortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+
+            newRedPortalScript.otherPortal = bluePortalScript;
+            bluePortalScript.otherPortal = newRedPortalScript;
+
+            currentRedPortal = newPortal;
+        }
+        else if (currentRedPortal != null && currentBluePortal != null)
+        {
+            PortalCollision newRedPortalScript = newPortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+            PortalCollision bluePortalScript = currentBluePortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+
+            newRedPortalScript.otherPortal = bluePortalScript;
+            bluePortalScript.otherPortal = newRedPortalScript;
+
+            Destroy(currentRedPortal);
+            currentRedPortal = newPortal;
+        }
     }
 
     void SpawnRedAtCursorLocation()
@@ -106,8 +171,20 @@ public class RedPortalGun : MonoBehaviour
     }
 
     void ShootBlue() {
-        SpawnBlueAtCursorLocation();
-        portalShootSound.Play();
+        // having trouble with portals shooting while paused. here's a temporty fix
+        if (!PauseMenu.isPaused)
+        {
+            // leaving the old fire mode in for debug purposes, and in case we want to go back and forth
+            if (debugPortalMode)
+            {
+                SpawnBlueAtCursorLocation();
+                portalShootSound.Play();
+            }
+            else
+            {
+                Debug.Log("New Fire Mode! Blue");
+            }
+        }
     }
 
     void SpawnBlueAtCursorLocation()
