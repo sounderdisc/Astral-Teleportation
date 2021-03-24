1bacfa0d73ade5371665178349e58588b7c3151f
sounderdisc <mattbhubbs@knights.ucf.edu>
2021-03-24 04:43:09 -0400

    Fixed placing portals via raycast

--- project/Astral Teleportation/Assets/Scripts/RedPortalGun.cs
+++ project/Astral Teleportation/Assets/Scripts/RedPortalGun.cs
@@ -6,7 +6,7 @@ using UnityEngine.Audio;
 public class RedPortalGun : MonoBehaviour
 {
     public float fireRate = 0f;
-    public LayerMask notToHit;
+    public LayerMask validTargets;
     public GameObject redPortalPrefab;
     public GameObject bluePortalPrefab;
     public GameObject currentRedPortal;
@@ -16,6 +16,10 @@ public class RedPortalGun : MonoBehaviour
 
     float timeToFire = 0f;
     Transform firepoint;
+
+    public bool onCooldown = false;
+    public float timeOnCooldown = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,14 +30,16 @@ public class RedPortalGun : MonoBehaviour
         }
         currentRedPortal = GameObject.Find("RedPortal");
         currentBluePortal = GameObject.Find("BluePortal");
+        validTargets = LayerMask.GetMask("Foreground");
         // uncomment next line to revert to old fire mode
-        debugPortalMode = true;
+        // debugPortalMode = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(fireRate == 0 ){
+        /*
+        if (fireRate == 0 ){
             if( Input.GetButtonDown ("Fire1")){
                 ShootRed();
             }
@@ -60,6 +66,34 @@ public class RedPortalGun : MonoBehaviour
                 ShootBlue();
             }
         }
+        */
+
+        // well, this fixes out double portal shooting problem at least
+        if (onCooldown)
+        {
+            timeOnCooldown += 1 * Time.deltaTime;
+            if (timeOnCooldown >= 0.1)
+            {
+                onCooldown = false;
+                timeOnCooldown = 0;
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                ShootRed();
+            }
+            if (Input.GetButtonDown("Fire2"))
+            {
+                ShootBlue();
+            }
+        }
+        // Debug.DrawLine(this.transform.position, this.transform.position + this.transform.right * 200, Color.green, 2);
+        //Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 direction = (Vector2)((worldMousePos - firepoint.position));
+        //direction.Normalize();
+        //Debug.DrawRay(this.transform.position, direction * 200, Color.green, 10);
     }
 
     void ShootRed() {
@@ -84,51 +118,35 @@ public class RedPortalGun : MonoBehaviour
     {
         // find fire direction for the shot
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (Vector2)((worldMousePos - firepoint.position));
+        Vector2 direction = (Vector2)((worldMousePos - this.transform.position));
         direction.Normalize();
+        Debug.DrawRay(this.transform.position, direction * 200, Color.red, 10);
         // do raycast, get location of hit
-        RaycastHit2D hit = Physics2D.Raycast(firepoint.position, direction);
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction, float.PositiveInfinity, validTargets);
         if (hit.collider == null)
         {
             return;
         }
-        Debug.Log("red raycast hit: " + hit.collider.name);
-        Vector3 spawnLocation = hit.collider.gameObject.transform.position;
-        spawnLocation.z = 662.0625f;
+        Debug.Log("red raycast hit & location: " + hit.collider.name + " @ " + hit.point);
+        // Vector3 spawnLocation = hit.collider.gameObject.transform.position;
+        Vector2 spawnLocation = hit.point;
+        // spawnLocation.z = 662.0625f;
         GameObject newPortal = Instantiate(redPortalPrefab, spawnLocation, Quaternion.identity);
-        
-        // so first, gonna copy/paste the reference fixing. i must make a "newPortal" before this section
-        // four possible states: no current red and no current blue portal, yes red but no blue, no red but yes blue, and both colors already exist
-        if (currentRedPortal == null && currentBluePortal == null)
+        // now lets fix our references so the portals are connected and actually move players
+        // first, let's see if the other color's gun has fired since the Start() function
+        currentBluePortal = GameObject.Find("BluePortal(Clone)");
+        if (currentBluePortal != null)
         {
-            currentRedPortal = newPortal;
+            PortalCollision blueScript = currentBluePortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+            PortalCollision redScript = newPortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
+            redScript.otherPortal = blueScript;
+            blueScript.otherPortal = redScript;
         }
-        else if (currentRedPortal != null && currentBluePortal == null)
+        if (currentRedPortal != null)
         {
             Destroy(currentRedPortal);
-            currentRedPortal = newPortal;
         }
-        else if (currentRedPortal == null && currentBluePortal != null)
-        {
-            PortalCollision newRedPortalScript = newPortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
-            PortalCollision bluePortalScript = currentBluePortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
-
-            newRedPortalScript.otherPortal = bluePortalScript;
-            bluePortalScript.otherPortal = newRedPortalScript;
-
-            currentRedPortal = newPortal;
-        }
-        else if (currentRedPortal != null && currentBluePortal != null)
-        {
-            PortalCollision newRedPortalScript = newPortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
-            PortalCollision bluePortalScript = currentBluePortal.GetComponent(typeof(PortalCollision)) as PortalCollision;
-
-            newRedPortalScript.otherPortal = bluePortalScript;
-            bluePortalScript.otherPortal = newRedPortalScript;
-
-            Destroy(currentRedPortal);
-            currentRedPortal = newPortal;
-        }
+        currentRedPortal = newPortal;
     }
 
     void SpawnRedAtCursorLocation()
@@ -182,6 +200,8 @@ public class RedPortalGun : MonoBehaviour
             }
             else
             {
+                // This functionality is being moved to it's own file because i need to attach a script to the blue arm so the raycast starts from the correct spot.
+                // ideally I'll come back later and clean up this 269 line mess of code, but for now, i dont want to break things
                 Debug.Log("New Fire Mode! Blue");
             }
         }
